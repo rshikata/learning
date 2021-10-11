fb4a5bd3de00dfc418384394b24c353a7b0bc073
shikata <ryota.shikata@jp.ricoh.com>
2021-10-11 17:22:13 +0900

    second commit(21/10/11)

--- archiver.js
+++ archiver.js
@@ -1,2 +1,27 @@
-const fs = require('fs');
-const archiver = require('archiver');
+
+// 必要なモジュールを読み込む
+const fs = require('fs');               // fs : ファイル入出力を扱う
+const archiver = require('archiver');   // アーカイブ生成用
+
+/*
+ _dirname : 現在のディレクトリのパス
+*/
+//console.log(__dirname );
+
+const output = fs.createWriteStream(_dirname + '/example.zip');
+const archive = archiver('zip',{
+    zlib: {level: 9 }
+})
+
+output.on('close', function(){
+    
+});
+
+output.on('end', function(){
+    
+});
+
+archive.pipe(output);   // 戻り値： 宛先ストリーム
+
+archive.globe('*.txt', {})
+archive.finalize();     // ファイルを圧縮
